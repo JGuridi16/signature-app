4b21554e11d8816788e2d7d76cc3962ff5eb7b5e
Jacsel Guridi <jguridi@cdevteam.com>
2023-06-10 18:36:46 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-wrapper">
+    <app-header />
+    <div class="h-100">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script setup>
+import { AppHeader } from '@/components/common'
+</script>
+
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import homeRoutes from './home';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    ...homeRoutes
+  ],
+});
+
+export default router;
